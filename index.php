b44d461a555c09bd97e9bc07bcdaf4438e690559
erik-gh <eriktecse@gmail.com>
2018-07-25 21:57:33 -0500

    Quite los headers en el index

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 
 <?php 
-//redireccionar a la vista de login
-//header ('Location: vistas/categoria.vista.php');
-//header ('Location: vistas/usuario.vista.php');
 header ('Location: views/usuario-view.php');
 ?>
